4e3c390b57c9464ff615422d2ee9d996563ec7cb
shaay98 <nrichardson567@icloud.com>
2025-04-24 02:46:58 +0000

    "started npm package an created functions"

---
+++ src/functions.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export function addTwo (num1, num2){
+    return "num1 + num2"
+};
+export function people (Alice, Keisha, Ryan){
+  return ["Alice, Keisha, Ryan"]
+};
+
+export function objects (laptop, cellphone, speaker){
+ return ["laptop, cellphone, speaker"]
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import { addTwo, people, objects} from "./functions.js";
+
+console.log(addTwo(13, 7));
+
+console.log(people("Alice", "Keisha", "Ryan"));
+
+console.log(objects("Windows", "iPhone8", "Alexa Speaker"));
